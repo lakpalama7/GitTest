6ab8996741512cca0b0ab93d8eb67f804eedf0f1
lakpa <sherpasvcet@gmail.com>
2020-02-22 21:32:50 +0530

    Initial commit

---
+++ app/src/main/java/com/liveinbits/githubtest/MainActivity.java
@@ -0,0 +1,18 @@
+package com.liveinbits.githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void sayHello(){
+        Log.i("Tag", "sayHello: how r u");
+    }
+}
