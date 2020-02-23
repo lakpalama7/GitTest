f5bba6d28bacb9060066c4fa3edffbcca5653ac7
lakpa <sherpasvcet@gmail.com>
2020-02-23 18:35:24 +0530

    add new content on branch1

--- app/src/main/java/com/liveinbits/githubtest/NewTest.java
+++ app/src/main/java/com/liveinbits/githubtest/NewTest.java
@@ -6,6 +6,7 @@ public class NewTest {
     private void show(){
         Log.i("TAG", "show: hi how are you");
         Log.i("branch01", "show: branch 01");
+        Log.i("PUll request", "show: Pull request branch 01");
     }
 
 }
