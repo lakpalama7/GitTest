4f72262af2ab5a28d712c3de6939de2910ac84f3
lakpa <sherpasvcet@gmail.com>
2020-02-23 12:59:42 +0530

    initial

--- app/src/main/java/com/liveinbits/githubtest/NewTest.java
+++ app/src/main/java/com/liveinbits/githubtest/NewTest.java
@@ -3,16 +3,5 @@ package com.liveinbits.githubtest;
 import android.util.Log;
 
 public class NewTest {
-    private  void show(){
-        Log.i("TAG", "show: hi");
-    }
-    private void display(){
-        
-    }
-    private void cal(){
-        
-    }
-    private  void go(){
-        
-    }
+   
 }
